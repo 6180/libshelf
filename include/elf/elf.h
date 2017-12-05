e587ceabf419f528407788a837bc42d8b95fb9fb
vee <vee@kun.fios-router.home>
2017-12-05 12:05:59 -0500

    Fix formatting

--- include/elf/elf.h
+++ include/elf/elf.h
@@ -176,87 +176,87 @@
  */
 std::unordered_map<int, std::string> e_machine_names = {
     {0, "No machine"},
-	{1, "AT&T WE 32100"},
-	{2, "SPARC"},
-	{3, "Intel 80386"},
-	{4, "Motorola 68000"},
-	{5, "Motorola 88000"},
-	{7, "Intel 80860"},
-	{8, "MIPS I Architecture"},
-	{9, "IBM System/370 Processor"},
-	{10, "MIPS RS3000 Little-endian"},
-	{15, "Hewlett-Packard PA-RISC"},
-	{17, "Fujitsu VPP500"},
-	{18, "Enhanced instruction set SPARC"},
-	{19, "Intel 80960"},
-	{20, "PowerPC"},
-	{21, "64-bit PowerPC"},
-	{22, "IBM System/390 Processor"},
-	{36, "NEC V800"},
-	{37, "Fujitsu FR20"},
-	{38, "TRW RH-32"},
-	{39, "Motorola RCE"},
-	{40, "Advanced RISC Machines ARM"},
-	{41, "Digital Alpha"},
-	{42, "Hitachi SH"},
-	{43, "SPARC Version 9"},
-	{44, "Siemens TriCore embedded processor"},
-	{45, "Argonaut RISC Core, Argonaut Technologies Inc."},
-	{46, "Hitachi H8/300"},
-	{47, "Hitachi H8/300H"},
-	{48, "Hitachi H8S"},
-	{49, "Hitachi H8/500"},
-	{50, "Intel IA-64 processor architecture"},
-	{51, "Stanford MIPS-X"},
-	{52, "Motorola ColdFire"},
-	{53, "Motorola M68HC12"},
-	{54, "Fujitsu MMA Multimedia Accelerator"},
-	{55, "Siemens PCP"},
-	{56, "Sony nCPU embedded RISC processor"},
-	{57, "Denso NDR1 microprocessor"},
-	{58, "Motorola Star*Core processor"},
-	{59, "Toyota ME16 processor"},
-	{60, "STMicroelectronics ST100 processor"},
-	{61, "Advanced Logic Corp. TinyJ embedded processor family"},
-	{62, "AMD x86-64 architecture"},
-	{63, "Sony DSP Processor"},
-	{64, "Digital Equipment Corp. PDP-10"},
-	{65, "Digital Equipment Corp. PDP-11"},
-	{66, "Siemens FX66 microcontroller"},
-	{67, "STMicroelectronics ST9+ 8/16 bit microcontroller"},
-	{68, "STMicroelectronics ST7 8-bit microcontroller"},
-	{69, "Motorola MC68HC16 Microcontroller"},
-	{70, "Motorola MC68HC11 Microcontroller"},
-	{71, "Motorola MC68HC08 Microcontroller"},
-	{72, "Motorola MC68HC05 Microcontroller"},
-	{73, "Silicon Graphics SVx"},
-	{74, "STMicroelectronics ST19 8-bit microcontroller"},
-	{75, "Digital VAX"},
-	{76, "Axis Communications 32-bit embedded processor"},
-	{77, "Infineon Technologies 32-bit embedded processor"},
-	{78, "Element 14 64-bit DSP Processor"},
-	{79, "LSI Logic 16-bit DSP Processor"},
-	{80, "Donald Knuth's educational 64-bit processor"},
-	{81, "Harvard University machine-independent object files"},
-	{82, "SiTera Prism"},
-	{83, "Atmel AVR 8-bit microcontroller"},
-	{84, "Fujitsu FR30"},
-	{85, "Mitsubishi D10V"},
-	{86, "Mitsubishi D30V"},
-	{87, "NEC v850"},
-	{88, "Mitsubishi M32R"},
-	{89, "Matsushita MN10300"},
-	{90, "Matsushita MN10200"},
-	{91, "picoJava"},
-	{92, "OpenRISC 32-bit embedded processor"},
-	{93, "ARC Cores Tangent-A5"},
-	{94, "Tensilica Xtensa Architecture"},
-	{95, "Alphamosaic VideoCore processor"},
-	{96, "Thompson Multimedia General Purpose Processor"},
-	{97, "National Semiconductor 32000 series"},
-	{98, "Tenor Network TPC processor"},
-	{99, "Trebia SNP 1000 processor"},
-	{100, "STMicroelectronics (www.st.com) ST200 microcontroller"},
+    {1, "AT&T WE 32100"},
+    {2, "SPARC"},
+    {3, "Intel 80386"},
+    {4, "Motorola 68000"},
+    {5, "Motorola 88000"},
+    {7, "Intel 80860"},
+    {8, "MIPS I Architecture"},
+    {9, "IBM System/370 Processor"},
+    {10, "MIPS RS3000 Little-endian"},
+    {15, "Hewlett-Packard PA-RISC"},
+    {17, "Fujitsu VPP500"},
+    {18, "Enhanced instruction set SPARC"},
+    {19, "Intel 80960"},
+    {20, "PowerPC"},
+    {21, "64-bit PowerPC"},
+    {22, "IBM System/390 Processor"},
+    {36, "NEC V800"},
+    {37, "Fujitsu FR20"},
+    {38, "TRW RH-32"},
+    {39, "Motorola RCE"},
+    {40, "Advanced RISC Machines ARM"},
+    {41, "Digital Alpha"},
+    {42, "Hitachi SH"},
+    {43, "SPARC Version 9"},
+    {44, "Siemens TriCore embedded processor"},
+    {45, "Argonaut RISC Core, Argonaut Technologies Inc."},
+    {46, "Hitachi H8/300"},
+    {47, "Hitachi H8/300H"},
+    {48, "Hitachi H8S"},
+    {49, "Hitachi H8/500"},
+    {50, "Intel IA-64 processor architecture"},
+    {51, "Stanford MIPS-X"},
+    {52, "Motorola ColdFire"},
+    {53, "Motorola M68HC12"},
+    {54, "Fujitsu MMA Multimedia Accelerator"},
+    {55, "Siemens PCP"},
+    {56, "Sony nCPU embedded RISC processor"},
+    {57, "Denso NDR1 microprocessor"},
+    {58, "Motorola Star*Core processor"},
+    {59, "Toyota ME16 processor"},
+    {60, "STMicroelectronics ST100 processor"},
+    {61, "Advanced Logic Corp. TinyJ embedded processor family"},
+    {62, "AMD x86-64 architecture"},
+    {63, "Sony DSP Processor"},
+    {64, "Digital Equipment Corp. PDP-10"},
+    {65, "Digital Equipment Corp. PDP-11"},
+    {66, "Siemens FX66 microcontroller"},
+    {67, "STMicroelectronics ST9+ 8/16 bit microcontroller"},
+    {68, "STMicroelectronics ST7 8-bit microcontroller"},
+    {69, "Motorola MC68HC16 Microcontroller"},
+    {70, "Motorola MC68HC11 Microcontroller"},
+    {71, "Motorola MC68HC08 Microcontroller"},
+    {72, "Motorola MC68HC05 Microcontroller"},
+    {73, "Silicon Graphics SVx"},
+    {74, "STMicroelectronics ST19 8-bit microcontroller"},
+    {75, "Digital VAX"},
+    {76, "Axis Communications 32-bit embedded processor"},
+    {77, "Infineon Technologies 32-bit embedded processor"},
+    {78, "Element 14 64-bit DSP Processor"},
+    {79, "LSI Logic 16-bit DSP Processor"},
+    {80, "Donald Knuth's educational 64-bit processor"},
+    {81, "Harvard University machine-independent object files"},
+    {82, "SiTera Prism"},
+    {83, "Atmel AVR 8-bit microcontroller"},
+    {84, "Fujitsu FR30"},
+    {85, "Mitsubishi D10V"},
+    {86, "Mitsubishi D30V"},
+    {87, "NEC v850"},
+    {88, "Mitsubishi M32R"},
+    {89, "Matsushita MN10300"},
+    {90, "Matsushita MN10200"},
+    {91, "picoJava"},
+    {92, "OpenRISC 32-bit embedded processor"},
+    {93, "ARC Cores Tangent-A5"},
+    {94, "Tensilica Xtensa Architecture"},
+    {95, "Alphamosaic VideoCore processor"},
+    {96, "Thompson Multimedia General Purpose Processor"},
+    {97, "National Semiconductor 32000 series"},
+    {98, "Tenor Network TPC processor"},
+    {99, "Trebia SNP 1000 processor"},
+    {100, "STMicroelectronics (www.st.com) ST200 microcontroller"},
 };
 
 /*
@@ -518,29 +518,29 @@ typedef struct {
  *   fixed-size entries. 
  */
 typedef struct {
-	uint32_t	sh_name;
-	uint32_t	sh_type;
-	uint32_t	sh_flags;
-	uint32_t	sh_addr;
-	uint32_t	sh_offset;
-	uint32_t	sh_size;
-	uint32_t	sh_link;
-	uint32_t	sh_info;
-	uint32_t	sh_addralign;
-	uint32_t	sh_entsize;
+    uint32_t    sh_name;
+    uint32_t    sh_type;
+    uint32_t    sh_flags;
+    uint32_t    sh_addr;
+    uint32_t    sh_offset;
+    uint32_t    sh_size;
+    uint32_t    sh_link;
+    uint32_t    sh_info;
+    uint32_t    sh_addralign;
+    uint32_t    sh_entsize;
 } Elf32_Shdr_t;
 
 typedef struct {
-	uint32_t	sh_name;
-	uint32_t	sh_type;
-	uint64_t	sh_flags;
-	uint64_t	sh_addr;
-	uint64_t	sh_offset;
-	uint64_t	sh_size;
-	uint32_t	sh_link;
-	uint32_t	sh_info;
-	uint64_t	sh_addralign;
-	uint64_t	sh_entsize;
+    uint32_t    sh_name;
+    uint32_t    sh_type;
+    uint64_t    sh_flags;
+    uint64_t    sh_addr;
+    uint64_t    sh_offset;
+    uint64_t    sh_size;
+    uint32_t    sh_link;
+    uint32_t    sh_info;
+    uint64_t    sh_addralign;
+    uint64_t    sh_entsize;
 } Elf64_Shdr_t;
 
 #endif
