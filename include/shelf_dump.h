aab897aa2d159b9956af2c97e0274bfeb22c7797
6180 <quine.618033988@gmail.com>
2018-05-04 10:10:02 -0400

    Update function names in shelf_dump.h

--- include/shelf_dump.h
+++ include/shelf_dump.h
@@ -3,9 +3,9 @@
 
 #include "shelf.h"
 
-extern void Elf_Dump_Ident(const Elf_Desc *desc);
-extern void Elf_Dump_Header(const Elf_Desc *desc);
-extern void Elf_Dump_Program_Headers(const Elf_Desc *desc);
-extern void Elf_Dump_Section_Headers(const Elf_Desc *desc);
+extern void Shelf_Dump_Ident(const Elf_Desc *desc);
+extern void Shelf_Dump_Header(const Elf_Desc *desc);
+extern void Shelf_Dump_Program_Headers(const Elf_Desc *desc);
+extern void Shelf_Dump_Section_Headers(const Elf_Desc *desc);
 
 #endif // ELF_5CD73F63
