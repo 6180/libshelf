7cf9cdf35be20ac8bb20609564cc5a75a8e7347f
Eevee <quine.618033988@gmail.com>
2018-05-20 23:53:40 -0400

    Remove dead code

--- src/shelf_profiler.c
+++ src/shelf_profiler.c
@@ -9,20 +9,6 @@ int profiler_level = 0;
  *   '/home/eevee/foo/bar/baz.h' to bar/baz.h. There are probably much cleaner
  *   ways to do this. If you know one please share~
  */
-// char *clean_filename(const char *file) {
-//     /* __FILE__ expands to a const * const char. The strdup removes
-//       the const pointer qualification. */
-//     char *tmp_str = strdup(file);
-//     char *tmp_base = tmp_str; // basename and dirname will modify the pointer we
-//     char *_dirname = basename(dirname(strdup(file)));
-//     char *_basename = basename(strdup(file));
-//     int tmplen = strlen(_dirname);
-//     strncpy(buffer, _dirname, BUFFER_LENGTH);
-//     buffer[tmplen] = '/';
-//     strncpy(buffer + tmplen + 1, _basename, BUFFER_LENGTH - tmplen - 2);
-//     return buffer;
-// }
-
 char *clean_filename(const char *file) {
     char *cpy = strdup(file);
     char *s = cpy + strlen(cpy);
